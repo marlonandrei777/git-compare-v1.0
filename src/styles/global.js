282e85fc6dec0c184813d8acab0f9241c143cf8c
Marlon Andrei <marlon-master@hotmail.com>
2021-03-08 22:27:39 -0300

    send repositories with props

---
+++ src/styles/global.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: 0; // tira a borda dentro do botão quando é clicado (Chrome)
+    }
+
+    body {
+        background: #9b65e6;
+        text-rendering: optimizeLegibility !important; //deixa a font mais legivel no google chrome
+        -webkit-font-smoothing: antialiased !important;
+        font-family: sans-serif;
+    }
+`;
+
+export default GlobalStyles;
